9516b9759e4cba84026847f426eb85f8265173d9
arieltoniatto <ariel.toniatto@gmail.com>
2022-10-21 17:37:30 -0300

    Configura o Dockerfile do frontend e backend e implementa a migration e model referente a Users req 01

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,25 @@
+import { Model, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  public id: number;
+  public username: string;
+  public role: string;
+  public email: string;
+  public password: string;
+}
+
+User.init({
+  username: STRING,
+  role: STRING,
+  email: STRING,
+  password: STRING,
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'User',
+  tableName: 'users',
+  timestamps: false,
+});
+
+export default User;
