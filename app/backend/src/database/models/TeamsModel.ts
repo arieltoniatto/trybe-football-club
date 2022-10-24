39b6e1e758a7140ebce9cb0fcae2790ce5c64276
arieltoniatto <ariel.toniatto@gmail.com>
2022-10-24 15:18:52 -0300

    cria a migration e model para a tabela teams

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,19 @@
+import { Model, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  public id: number;
+  public teamName: string;
+}
+
+Team.init({
+  teamName: STRING,
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'Team',
+  tableName: 'teams',
+  timestamps: false,
+});
+
+export default Team;
