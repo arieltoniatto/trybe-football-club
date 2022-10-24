ba20c0a7907016d6d6db66ab9f6f247e84d2ace4
arieltoniatto <ariel.toniatto@gmail.com>
2022-10-24 15:04:30 -0300

    Cria testes referentes ao end point Login e login/validate

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -7,36 +7,83 @@ import { app } from '../app';
 import UserModel from '../database/models/UserModel';
 
 import { Response } from 'superagent';
+import User from '../database/models/UserModel';
+
+import { findOne, loginBody, invalidEmail } from './mocks/loginMocks';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Testing Login endpoint', () => {
+describe('Testing Login endpoint success', () => {
 
   let chaiHttpResponse: Response;
   let token: string;
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
+  beforeEach(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(findOne as User);
+  });
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+  afterEach(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
 
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
+  it('Request OK', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .post('/login')
+       .send(loginBody)
 
-  //   expect(...)
-  // });
-
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.have.property('token')
   });
 });
+
+describe('Testing Login endpoint errors', () => {
+  let chaiHttpResponse: Response;
+
+  beforeEach(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(null);
+  });
+
+  afterEach(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
+
+  it('Error if email is invalid', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send(invalidEmail)
+
+    expect(chaiHttpResponse.status).to.be.equal(401);
+    expect(chaiHttpResponse.body.message).to.be.equal('Incorrect email or password')
+  })
+})
+
+describe('Testing login/validade endpoint success', () => {
+  let chaiHttpResponse: Response;
+
+  beforeEach(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(findOne as User);
+  });
+
+  afterEach(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
+
+  it('Request OK', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/login/validate')
+      .set('authorization', 'eyJhbGciOiJIUzI1NiJ9.YWRtaW5AYWRtaW4uY29t.s1U6I8B6x_9eLeJyb9PdjTz1JbNXo57xor-T1493RW0')
+
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.be.equal('admin')
+  })
+})
---
+++ app/backend/src/tests/mocks/loginMocks.ts
@@ -0,0 +1,17 @@
+export const findOne = {
+  id: 1,
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: '$2a$12$Y5poB8FPOsUPv5EwcW7kdO3YEuJFP.YMq8DTIEFIVmaI3ttuPLSTu',
+}
+
+export const loginBody = {
+  email: 'admin@admin.com',
+  password: 'admin123',
+}
+
+export const invalidEmail = {
+  email: 'admin.com',
+  password: 'admin123',
+}
