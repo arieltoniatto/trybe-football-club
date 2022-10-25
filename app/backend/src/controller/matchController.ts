e9cfe4c72ef7cfd070be35d4a70e760d7cd807c6
arieltoniatto <ariel.toniatto@gmail.com>
2022-10-25 14:25:26 -0300

    Desenvolvido endpoint que filtra as partidas em andamento e finalizadas. req 20 e 21

--- app/backend/src/controller/matchController.ts
+++ app/backend/src/controller/matchController.ts
@@ -4,9 +4,23 @@ import MatchService from '../services/matchService';
 export default class MatchController {
   constructor(private matchService = new MatchService()) {}
 
-  public getAll = async (_req: Request, res: Response) => {
+  public getAll = async (req: Request, res: Response) => {
+    const { inProgress } = req.query;
+    if (inProgress) return this.inProg(req, res);
     try {
       const response = await this.matchService.allMatches();
+      return res.status(200).json(response);
+    } catch (err) {
+      return res.send(err);
+    }
+  };
+
+  public inProg = async (req: Request, res: Response) => {
+    console.log(req.query);
+    try {
+      const { inProgress } = req.query;
+      const response = await this.matchService.inProgress(inProgress as string);
+      if (!response) return res.status(404).json({ message: 'Not found' });
       return res.status(200).json(response);
     } catch (err) {
       return res.send(err);
--- app/backend/src/services/matchService.ts
+++ app/backend/src/services/matchService.ts
@@ -6,7 +6,23 @@ export default class MatchService {
     const response = await Match.findAll(
       { include: [{ all: true, attributes: ['teamName'] }] },
     );
-
     return response;
+  };
+
+  public inProgress = async (query: string): Promise<IMatch[] | null> => {
+    let isInProgress;
+    if (query === 'true') isInProgress = true;
+    if (query === 'false') isInProgress = false;
+
+    const result = await Match.findAll({
+      where: {
+        inProgress: isInProgress,
+      },
+      include: [{ all: true, attributes: ['teamName'] }],
+    });
+
+    if (!result) return null;
+
+    return result;
   };
 }
